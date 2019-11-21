d1dfccdca46c8f4d16f0213799e786d40f83e324
DamaiDaMai <357086556@qq.com>
2019-11-21 22:24:11 +0800

    https://github.com/DaiMaDaiMa/PythonStudy.git

---
+++ hellopython.py
@@ -0,0 +1,16 @@
+class HelloPython(object):
+    """docstring for HelloPython"""
+
+    def __init__(self):
+        print("这是初始话方法")
+
+    def main(self):
+        print("HelloPython")
+
+
+if __name__ == "__main__":
+    hp = HelloPython()
+    hp.main()
+
+		
+		
